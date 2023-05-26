79d23de93bc919dc6ddf682753853d110df09c37
JNAULAS <93282712+JNAULAS@users.noreply.github.com>
2023-05-26 11:23:29 -0500

    Update ICalculator.java
    Update

--- tdd/src/main/java/ec/edu/ups/tdd/calculator/ICalculator.java
+++ tdd/src/main/java/ec/edu/ups/tdd/calculator/ICalculator.java
@@ -5,4 +5,10 @@ public interface ICalculator {
      * Definimos los los metodos
      */
     public int addition (int a, int b);
+    
+    public int subtraction(int i, int i1);
+
+    public int multiplication(int i, int i1);
+
+    public int division(int i, int i1);
 }
